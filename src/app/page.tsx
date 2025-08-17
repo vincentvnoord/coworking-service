b14c4721e84366052585e24ffcd3504095d65049
Vincent van Noord <vannoord.vincent@gmail.com>
2025-08-17 14:52:38 +0200

    Hero Section (#2)
    * ADD: Image placeholders, hero text and search button
    
    * ADD: Workspace select for search
    
    * ADD: Desk icon
    
    * ADD: Search select finished, added all icons
    
    * ADD: Location Input
    
    * ADD: Hero images with labels
    
    * IMP: Put hero in its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import { Header } from "@/components/header";
+import { Header } from "@/components/main-page/header";
+import { Hero } from "@/components/main-page/hero";
 
 export default function Home() {
   return (
     <div className="w-screen flex flex-col items-center px-6">
       <Header />
-
-      <h1 className="w-full max-w-screen-xl p-6 text-4xl font-bold">
-        Design Your Workday
-      </h1>
+      <Hero />
     </div>
   );
 }
+
