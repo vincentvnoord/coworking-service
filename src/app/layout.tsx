31ac6e7b72980226dba592ab1119ff033d7117e1
Vincent van Noord <vannoord.vincent@gmail.com>
2025-08-19 19:34:02 +0200

    ADD: Footer description and email signup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Link from "next/link";
 import LogoIcon from "@/components/icons/logo";
+import Button from "@/components/button";
 
 export const metadata: Metadata = {
   title: "WorkMosaic",
@@ -19,15 +20,36 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
-        <footer>
-          <Link
-            href={"/"}
-            className="text-primary-400 flex gap-2 items-center">
-            <LogoIcon width={32} height={32} />
-            <span className="font-bold text-xl">
-              WorkMosaic
-            </span>
-          </Link>
+        <footer className="w-screen p-3 sm:p-6 pb-12 flex flex-col items-center border-t-2 border-primary-100">
+          <div className="max-w-screen-xl w-full flex flex-col gap-4">
+            <Link
+              href={"/"}
+              className="flex w-fit gap-2 items-center">
+              <LogoIcon width={32} height={32} />
+              <span className="font-bold">
+                WorkMosaic
+              </span>
+            </Link>
+
+            <div className="flex gap-4 items-center">
+              <p className="w-full max-w-md text-gray-6">
+                We provide flexible workspace solutions, private offices, and a vibrant community for professionals and businesses to connect, collaborate, and grow.
+              </p>
+
+              <div className="w-full flex gap-4 justify-end">
+                <input
+                  className="p-2 px-4 text-lg bg-gray-2 rounded-full min-w-96"
+                  placeholder="Enter your email"
+                  type="text"
+                />
+                <Button className="min-w-34 px-8 py-3">
+                  Sign up for newsletter
+                </Button>
+              </div>
+
+            </div>
+
+          </div>
         </footer>
       </body>
     </html>
