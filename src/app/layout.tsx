b12ad1646944ba8ea7c47ee5d316e530dcaa7fd1
Vincent van Noord <vannoord.vincent@gmail.com>
2025-08-19 17:42:30 +0200

    FIX: Login and signup buttons width and size differences

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
+import LogoIcon from "@/components/icons/logo";
 
 export const metadata: Metadata = {
   title: "WorkMosaic",
@@ -17,6 +19,16 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <footer>
+          <Link
+            href={"/"}
+            className="text-primary-400 flex gap-2 items-center">
+            <LogoIcon width={32} height={32} />
+            <span className="font-bold text-xl">
+              WorkMosaic
+            </span>
+          </Link>
+        </footer>
       </body>
     </html>
   );
