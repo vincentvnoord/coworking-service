679efe45b26185035adbf4b2d97f41fbed29f336
Vincent van Noord <vannoord.vincent@gmail.com>
2025-08-21 17:02:32 +0200

    ADD: Mobile header

--- src/components/main-page/header.tsx
+++ src/components/main-page/header.tsx
@@ -1,11 +1,18 @@
 import Button from "@/components/button";
 import LogoIcon from "@/components/icons/logo";
 import Link from "next/link";
+import { MobileHeader } from "./mobile-header";
+
+export const headerElements = [
+  { href: "/locations", label: "Locations" },
+  { href: "/community", label: "Community" },
+  { href: "/contacts", label: "Contacts" },
+]
 
 export const Header = () => {
 
   return (
-    <header className="flex items-center pt-8 justify-between w-full max-w-screen-xl">
+    <header className="flex min-h-18 items-center lg:pt-8 justify-between w-full max-w-screen-xl">
       <div className="flex items-center gap-8">
         <Link
           href={"/"}
@@ -15,14 +22,22 @@ export const Header = () => {
             WorkMosaic
           </span>
         </Link>
-        <nav className="flex items-center gap-8 space-x-4 p-6 text-lg">
-          <Link href="/locations" className="text-gray-700 hover:text-gray-900">Locations</Link>
-          <Link href="/community" className="text-gray-700 hover:text-gray-900">Community</Link>
-          <Link href="/contacts" className="text-gray-700 hover:text-gray-900">Contacts</Link>
+        <nav className="hidden sm:flex items-center gap-8 space-x-4 p-6 text-lg">
+          {
+            headerElements.map((element) => (
+              <Link
+                key={element.href}
+                href={element.href}
+                className="text-gray-700 hover:text-gray-900"
+              >
+                {element.label}
+              </Link>
+            ))
+          }
         </nav>
       </div>
 
-      <div className="text-lg font-medium flex gap-3 items-center h-full">
+      <div className="hidden text-lg font-medium lg:flex gap-3 items-center h-full">
         <Button className="min-w-34 px-8 py-3" variant="outline">
           Log in
         </Button>
@@ -30,6 +45,8 @@ export const Header = () => {
           Sign up
         </Button>
       </div>
+
+      <MobileHeader />
     </header>
   )
 }
---
+++ src/components/main-page/mobile-header.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { MenuIcon, XIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import Button from "@/components/button";
+import { headerElements } from "./header";
+import Link from "next/link";
+
+
+export const MobileHeader = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [open]);
+
+  return (
+    <div className="lg:hidden flex">
+      <button onClick={() => setOpen(true)}>
+        <MenuIcon size={32} />
+      </button>
+      <div className={`${open ? "pointer-events-auto" : "pointer-events-none opacity-0"} transition-opacity flex flex-col p-6 fixed inset-0 bg-white z-50`}>
+        <button onClick={() => setOpen(false)} className="self-end">
+          <XIcon size={32} />
+        </button>
+        <div className="text-lg justify-between pb-12 pt-12 font-medium flex flex-col w-full gap-3 items-center h-full">
+          <nav className="flex flex-col items-center gap-8 text-lg">
+            {
+              headerElements.map(({ href, label }) => (
+                <Link
+                  key={href}
+                  href={href}
+                  className="text-gray-700 hover:text-gray-900"
+                >
+                  {label}
+                </Link>
+              ))
+            }
+          </nav>
+
+          <div className="flex flex-col gap-4 w-full max-w-xs">
+            <Button className="min-w-34 px-8 py-3 w-full" variant="outline">
+              Log in
+            </Button>
+            <Button className="min-w-34 px-8 py-3 w-full">
+              Sign up
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
