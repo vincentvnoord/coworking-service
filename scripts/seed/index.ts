ae5d9ccf8f2b1f66da615181d2993d2fde16e942
Vincent van Noord <vannoord.vincent@gmail.com>
2025-08-25 17:04:40 +0200

    ADD: Drizzle ORM setup and initial migration

---
+++ scripts/seed/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { eq } from 'drizzle-orm';
+import { usersTable } from '@/db/schema';
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+
+  await db
+    .update(usersTable)
+    .set({
+      name: 'John Doe',
+    })
+    .where(eq(usersTable.email, user.email));
+
+  console.log('User info updated!')
+}
+
+main();
+
